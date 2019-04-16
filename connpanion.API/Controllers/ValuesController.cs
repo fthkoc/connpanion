b6198befa789029777ef32b65109d59bbf2b3ec3
icecub3 <fkoc.041@gmail.com>
2019-04-16 23:55:01 +0300

    Login/Logout interface

--- connpanion.API/Controllers/ValuesController.cs
+++ connpanion.API/Controllers/ValuesController.cs
@@ -22,6 +22,7 @@ namespace connpanion.API.Controllers
         }
 
         // GET api/values
+        [AllowAnonymous]
         [HttpGet]
         public async Task<IActionResult> GetValues()
         {
@@ -30,6 +31,7 @@ namespace connpanion.API.Controllers
         }
 
         // GET api/values/5
+        [AllowAnonymous]
         [HttpGet("{id}")]
         public async Task<IActionResult> GetValue(int id)
         {
